0d9348ccfe5d30dce9b65409ccfd590274e28814
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-25 13:13:42 -0300

    ♻️ move LoadProductByName use case result to model

--- src/domain/use-cases/load-product-by-name.ts
+++ src/domain/use-cases/load-product-by-name.ts
@@ -1,11 +1,9 @@
+import { ProductModel } from '../models/product'
+
 export interface LoadProductByName {
   load: (name: string) => Promise<LoadProductByName.Result>
 }
 
 export namespace LoadProductByName {
-  export type Result = {
-    name: string
-    price: number
-    quantity: number
-  }
+  export type Result = ProductModel
 }
