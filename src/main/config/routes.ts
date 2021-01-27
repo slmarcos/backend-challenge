b403581b56cf4f40976b6ef1d2e77736c5ce8393
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-27 12:24:37 -0300

    ✨ remove router prefix

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,7 +3,7 @@ import { readdirSync } from 'fs'
 
 export default (app: Express): void => {
   const router = Router()
-  app.use('/api', router)
+  app.use(router)
   readdirSync(`${__dirname}/../routes`).map(async file => {
     if (!file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router)
