d4765055b9d2ae4111756c20c2173d0a6a7bda3f
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-27 12:10:50 -0300

    🐛 fix error message in the adaptRoute

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -13,7 +13,7 @@ export const adaptRoute = (controller: Controller) => {
       res.status(httpResponse.statusCode).json(httpResponse.body)
     } else {
       res.status(httpResponse.statusCode).json({
-        error: httpResponse.body.message
+        error: httpResponse.error.message
       })
     }
   }
