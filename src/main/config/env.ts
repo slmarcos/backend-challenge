ce8499b047184effd04214daf8b3d6d2f4beeba6
Marcos Lopes <sl.marcos15@hotmail.com>
2021-01-27 01:33:32 -0300

    ✨ add support to environment variables

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,11 @@
-export default {
-  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/backend-delivery',
-  port: process.env.PORT ?? 3010,
+import dotenv from 'dotenv'
+dotenv.config()
 
-  stockServiceUrl: process.env.STOCK_SERVICE_USER ?? 'amqp://localhost:5672/',
+export default {
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/db-test',
+  port: process.env.PORT ?? 3011,
+
+  stockServiceUrl: process.env.STOCK_SERVICE_URL ?? 'amqp://localhost:5672/',
   stockServiceUser: process.env.STOCK_SERVICE_USER ?? 'guest',
   stockServicePassword: process.env.STOCK_SERVICE_PASSWORD ?? 'guest'
 }
